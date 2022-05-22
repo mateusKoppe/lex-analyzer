f695d3a3c2f61975b4f1ceda8591ac74dbf12e96
Mateus Koppe <mateus.koppe@gmail.com>
2022-05-21 23:12:05 -0300

    DFA - Implement from NFA

--- finite_automaton/dfa.py
+++ finite_automaton/dfa.py
@@ -1,124 +1,116 @@
+from __future__ import annotations
+from collections import deque
+from typing import Dict
 from terminaltables import AsciiTable
 
 from finite_automaton.nfa import NFA
+from finite_automaton.state import State
+
+class RemapQueue:
+    def __init__(self) -> None:
+        self.discovered = set()
+        self.to_discover = deque()
+        self.state_to_create = 1
+        self.remap_state: Dict[str, set[str]] = {}
+
+    def push_to_discovery(self, state, merged_states = None):
+        if not merged_states:
+            merged_states = { state }
+
+        if ((state, merged_states) not in self.to_discover) and state not in self.discovered:
+            self.to_discover.append((state, merged_states))
+    
+    def set_discovered(self, state):
+        self.discovered.add(state)
+
+    def set_remap(self, deterministic_state, merged_states):
+        self.remap_state[deterministic_state] = merged_states
+
+    def pop_to_discover(self):
+        try:
+            next_tuple = self.to_discover.popleft()
+            return next_tuple
+        except IndexError:
+            return None
+
+    def state_by_merged_states(self, states):
+        try:
+            map_state = [map_state for map_state, merged_states in self.remap_state.items() if merged_states==states]
+            return map_state[0]
+        except IndexError:
+            deterministic_state = str(self.state_to_create)
+            
+            self.push_to_discovery(deterministic_state, states)
+            self.set_remap(deterministic_state, states)
+            self.state_to_create += 1
+            return deterministic_state
+
 
 class DFA:
-    @staticmethod
-    def from_NFA(nfa):
-        dfa = DFA()
-        states = DFA.eliminate_indeterminism(nfa.states)
-        for key, expression in states.items():
-            dfa.states[DFA.index_to_production_name(key)] = {
-                **expression,
-                "productions": {non_terminal: DFA.index_to_production_name(terminal.pop())
-                    for non_terminal, terminal in expression["productions"].items()
-                }
-            }
+    @classmethod
+    def from_NFA(cls, nfa: NFA) -> DFA:
+        dfa = cls()
 
-        return dfa
+        remap_queue = RemapQueue()
+        remap_queue.push_to_discovery(nfa.initial_state.name)
+        state_tuple = remap_queue.pop_to_discover()
+        while state_tuple:
+            state_name, states_to = state_tuple
+            new_state = State(state_name)
+            dfa.add_state(new_state)
+            remap_queue.set_discovered(state_name)
 
-    @staticmethod
-    def index_to_production_name(num):
-        # Because S should be moved to the first index
-        letters = ["S", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "T", "U", "V", "W", "Y", "Z"]
+            for state_to in list(states_to):
+                state = nfa.states[state_to]
 
-        index = num - 1
-        size = len(letters)
-        if index < size:
-            return letters[index]
-        else:
-            rigth_letter = letters[index % size]
-            left = DFA.index_to_production_name(index // size)
-            return left + rigth_letter
-
-    @staticmethod
-    def format_productions(nfa):
-        dfa = {}
-        for index, item in nfa.items():
-            productions = dict(zip(
-                item["productions"].keys(),
-                list(map(
-                    lambda x: list(map(DFA.index_to_production_name, sorted(x))),
-                    item["productions"].values()
-                ))
-            ))
-                
-            dfa[DFA.index_to_production_name(index)] = {
-                **item,
-                "productions": productions
-            }
-        return dfa
-
-    @staticmethod
-    def merge_productions(nfa, productions):
-        new_production = {"productions": {}, "is_final": False}
-        for key in productions:
-            production = nfa[key]
-            new_production["is_final"] |= production["is_final"]
-            new_production["from"] = set(productions)
-
-            for non_terminal, terminals in production["productions"].items():
-                new_terminals = new_production["productions"].get(non_terminal, set())
-                new_terminals = new_terminals.union(terminals)
-                new_production["productions"][non_terminal] = new_terminals
-
-        return new_production
-
-    def search_production_rule(nfa, productions):
-        for key, items in nfa.items():
-            if items.get("from", False) == set(productions):
-                return key
-        return None
-
-    def eliminate_indeterminism(nfa):
-        dfa = nfa.copy()
-
-        has_change = False
-        for key, items in nfa.items():
-            for non_terminal, terminals in items["productions"].items():
-                if len(terminals) > 1:
-                    has_change = True
-                    production_rule = DFA.search_production_rule(dfa, terminals)
-                    if production_rule:
-                        dfa[key]["productions"][non_terminal] = {production_rule}
+                for terminal, states in state.transitions.items():
+                    is_deterministic = len(states) == 1
+                    if is_deterministic:
+                        transition_state = list(states)[0]
+                        # TODO: This can still cause indeterminism 
+                        new_state.add_transition(terminal, transition_state)
+                        remap_queue.push_to_discovery(transition_state)
                     else:
-                        last_key = sorted(dfa.keys()).pop() + 1
-                        dfa[last_key] = DFA.merge_productions(dfa, terminals)
-                        dfa[key]["productions"][non_terminal] = {last_key}
+                        maped_state = remap_queue.state_by_merged_states(states)
+                        new_state.add_transition(terminal, maped_state)
 
-        if has_change:
-            return DFA.eliminate_indeterminism(dfa)
+            state_tuple = remap_queue.pop_to_discover()
 
-
-        for key, items in nfa.items():
-            if dfa[key].get("from"):
-                del dfa[key]["from"]
-
-
-        return NFA.remove_useless_expressions(dfa)
+        dfa.initial_state = dfa.states[nfa.initial_state.name]
+        return dfa
 
     def __init__(self):
-        self.states = {}
+        self.states: Dict[str, State] = {}
+        self.initial_state: State = None
+
+    def add_state(self, state: State):
+        if state.name in self.states:
+            raise Exception(f"State {state.name} already exist")
+
+        self.states[state.name] = state
 
     def asci_table(self):
         header = ['/']
-        for index, expressions in self.states.items():
-            for letter in expressions["productions"].keys():
+        for state in self.states.values():
+            for letter in state.transitions.keys():
                 try:
                     header.index(letter)
                 except ValueError:
                     header.append(letter)
         rows = [header]
 
-        for index, expression in self.states.items():
+        for state in self.states.values():
             row = []
             for column in header:
                 if column == '/':
-                    row.append(index + ("*" if expression["is_final"] else ""))
+                    row.append(
+                    state.name + (
+                        "*" if state.is_final else ""
+                    ))
                     continue
 
                 try:
-                    row.append(expression["productions"][column])
+                    row.append(", ".join(state.transitions[column]))
                 except KeyError:
                     row.append('-')
             rows.append(row)
